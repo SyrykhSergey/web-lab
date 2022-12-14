c70aceb5f842bf074986a33aabb33d6ea94ae539
Sergey Syrykh <syrykh.ser@gmail.com>
2022-12-14 22:48:35 +0700

    Исправлен визуал рейтинга. Добавлен визуал покупки

--- src/Components/UI/ratings.vue
+++ src/Components/UI/ratings.vue
@@ -1,13 +1,31 @@
 <template>
-
+  <div class="ratings">
+    <ion-icon v-if="star === 1" name="star"></ion-icon>
+    <ion-icon v-if="star === 0" name="star-outline"></ion-icon>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ratings"
+  name: "ratings",
+
+  props:{
+    star:{
+      type: Number,
+      required: true
+    }
+  }
+
+
 }
 </script>
 
 <style scoped>
-
+.ratings{
+  display: flex;
+  flex-wrap: nowrap;
+  color: gold;
+  width: 14px;
+  height: 14px;
+}
 </style>
--- src/Components/catalog-item.vue
+++ src/Components/catalog-item.vue
@@ -2,22 +2,64 @@
 <div class="catalog-item">
   <img class="dishesImage" :src=post.image>
   <div class="descriptionItem">
-    <p4>{{post.name}}</p4>
-    <p6>Категория блюда: {{post.category}}</p6>
-    <p6>{{post.rating}}</p6>
+    <p4>{{post.name}}<br></p4>
+    <p6>Категория блюда: {{post.category}}<br></p6>
+    <div class="ratingsComp">
+      <ratings
+      v-for="starItem in starCount"
+      :star="starItem"
+      >
+
+      </ratings>
+    </div>
+
     <p>{{post.description}}</p>
+    <div class="buying">
+      <p5>Цена: {{post.price}}</p5>
+      <ion-icon class="buyPlace" name="cart-outline"></ion-icon>
+    </div>
   </div>
 </div>
 </template>
 
 <script>
+import ratings from "@/Components/UI/ratings.vue";
+
 export default {
   name: "catalog-item",
+  data(){
+    return{
+      stars: 0,
+      starCount:[],
+    }
+  },
+  components: {ratings},
   props:{
     post:{
       type: Object,
       required: true
     }
+  },
+  methods:{
+    countStars(){
+      this.starCount = [];
+      this.stars = Math.floor(Number(this.post.rating));
+
+      let i = 0;
+      while(i < this.stars){
+        this.starCount.push(1);
+        i++;
+      }
+      i = 10-this.stars;
+      while(i > 0){
+        this.starCount.push(0);
+        i--;
+      }
+    }
+
+  },
+  mounted() {
+    this.countStars()
   }
 }
 </script>
@@ -31,11 +73,28 @@ export default {
   padding: 0px;
   margin: 10px;
   width: 150px;
-  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
+  border: 1px solid gray;
   display: flex;
   flex-direction: column;
   flex-wrap: nowrap;
 }
+.ratingsComp{
+  display: flex;
+  flex: border-box;
+  flex-direction: row;
+}
+.buying{
+  float: bottom;
+  background: #a3a2a2;
+}
+.buyPlace{
+
+  background: seagreen;
+  border-radius: 5px;
+  float: right;
+
+}
+
 p{
   font-size: 12px;
 }
